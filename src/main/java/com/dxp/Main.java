ee1c05b0cc9291ae4cf7159ebb7a6b20bd1170a8
Denis <prundus25@iespabloserrano.com>
2026-01-13 12:29:27 +0100

    add list functionality

--- src/main/java/com/dxp/Main.java
+++ src/main/java/com/dxp/Main.java
@@ -1,6 +1,13 @@
 package com.dxp;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,14 +18,18 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    static ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+    public static ListManager manager2 = new ListManager();
     final Color customGrayBlue = Color.rgb(47, 49, 54);
+    static boolean dataImp = false;
+
     public static void main(String[] args) {
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         Scene scene = new Scene(root, customGrayBlue);
         Image icon = new Image(new FileInputStream("src/main/java/com/dxp/LIcon.png"));
@@ -28,5 +39,18 @@ public class Main extends Application {
 
         stage.setScene(scene);
         stage.show();
+
+        
+    }
+
+    public void importData(){
+        try {
+            manager2.lists = mapper.readValue(new File("target/generated-sources/data.json"), mapper.getTypeFactory().constructCollectionType(ArrayList.class, MyList.class));
+            System.out.println("Data loaded successfully.");
+            dataImp = true;
+        } catch (Exception e) {
+            System.err.println("Error. No data could be loaded: ");
+            dataImp = false;
+        }
     }
 }
